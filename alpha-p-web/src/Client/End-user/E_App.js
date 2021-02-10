6f8991d55b7ec7f6b9be9b98e246c96f95c6179b
Abdulmohsen2 <63165927+Abdulmohsen2@users.noreply.github.com>
2021-02-10 23:31:50 +0300

    sign-Up page
    - Finish sign-up page
    - the colors will chsnge
    - if there any problem with form please inform me

--- alpha-p-web/src/Client/End-user/E_App.js
+++ alpha-p-web/src/Client/End-user/E_App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-// import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './scss/style.scss';
 import Login from './views/Login';
+import SignUp from './views/Sign-up';
+
 export class EApp extends Component {
     render() {
         return (
             <div>
-                <Login/>
+                <SignUp/>
             </div>
         )
     }
