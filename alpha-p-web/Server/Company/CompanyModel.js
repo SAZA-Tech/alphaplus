a7bf688870dc9437c15e92354e9e58e88282ea19
AzizAlameer <zoozz5747@gmail.com>
2021-02-17 20:37:21 +0300

    data types

--- alpha-p-web/Server/Company/CompanyModel.js
+++ alpha-p-web/Server/Company/CompanyModel.js
@@ -3,10 +3,10 @@ const { model, Schema } = require('mongoose');
 const CompanySchema = new Schema({
   symbol: String,
   exchange:String,
-  open: String,
-  high: String,
-  low: String,
-  close:String
+  open: Number,
+  high: Number,
+  low: Number,
+  close:Number
 });
 
 module.exports = model('Company', CompanySchema);
