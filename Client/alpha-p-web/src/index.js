5a15e77fc50b91b3ded960a1a5d2804434ea0d69
Saleh S | SZ4m <45066747+SZ4M@users.noreply.github.com>
2021-02-18 10:04:57 +0300

    Re set-up the code
    Devided the app into two folders
    
    1 Server
    
    2 Client  - Containing :
     - alpha-p-web ( For End User )
     - alpha-p-admin (For Admin User )

---
+++ Client/alpha-p-web/src/index.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+
+
+import { Provider } from "react-redux";
+
+import ApolloProvider from "./ApolloProvider";
+
+
+ReactDOM.render(ApolloProvider, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
