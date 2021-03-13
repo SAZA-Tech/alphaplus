9fc142b0a40a1f73ca777049ee301c5766c89269
Saleh S | SZ4m <45066747+SZ4M@users.noreply.github.com>
2021-03-13 18:16:18 +0300

    Removed upload resolvers

--- server/GraphQl/resolvers/index.js
+++ server/GraphQl/resolvers/index.js
@@ -1,7 +1,6 @@
 const contentResolvers = require("./content_resolvers");
 const companyResovlers = require("./company_resolvers");
 const userResovlers = require("./user_resovlers");
-const uploadResolvers = require("./uploadResolvers");
 module.exports = {
   Article: {
     commentCount: (parent) => parent.articleComments.length,
@@ -10,12 +9,10 @@ module.exports = {
   Query: {
     ...userResovlers.Query,
     ...contentResolvers.Query,
-    ...uploadResolvers.Query,
   },
   Mutation: {
     // ...companyResovlers.Mutation,
     ...userResovlers.Mutation,
     ...contentResolvers.Mutation,
-    ...uploadResolvers.Mutation,
   },
 };
